2920de1869437b9e21747ec734fec8980cc8dfcf
VMurtazin <obdorsk@gmail.com>
2023-02-27 22:38:54 +0500

    addDaoMethods

--- src/main/java/ru/murtazin/crud/dao/UserDaoImpl.java
+++ src/main/java/ru/murtazin/crud/dao/UserDaoImpl.java
@@ -1,37 +1,54 @@
 package ru.murtazin.crud.dao;
 
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Repository;
 import ru.murtazin.crud.model.User;
 
+import javax.persistence.EntityManager;
+import javax.persistence.Query;
 import java.util.List;
 
 @Repository
 public class UserDaoImpl implements UserDao{
 
+    private EntityManager entityManager;
+
+    @Autowired
+    public UserDaoImpl(EntityManager entityManager) {
+        this.entityManager = entityManager;
+    }
 
     @Override
+    @SuppressWarnings("unchecked")
     public List<User> getAllUsers() {
-        return null;
+        Query query = entityManager.createNativeQuery("select * from users", User.class);
+        List<User> list = query.getResultList();
+        return list;
     }
 
     @Override
     public void add(User user) {
 
+        entityManager.persist(user);
     }
 
     @Override
     public void delete(int id) {
 
+        User user=entityManager.find(User.class,id);
+        entityManager.remove(user);
     }
 
     @Override
     public void edit(User updatedUser) {
 
+        entityManager.merge(updatedUser);
     }
 
     @Override
     public User getById(int id) {
-        return null;
+
+        return entityManager.find(User.class,id);
     }
 }
